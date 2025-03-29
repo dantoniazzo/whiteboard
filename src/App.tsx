bfa3bc4a8690f7eb0db7093f9502f814e396f128
dantoniazzo <85890036+dantoniazzo@users.noreply.github.com>
2025-03-29 14:54:58 +0100

    Feature: add right rail component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Editor } from '_widgets/Editor';
 import { LeftRail } from '_widgets/LeftRail';
+import { RightRail } from '_widgets/RightRail';
 
 function App() {
   return (
     <div className="w-screen h-screen bg-gray-900">
       <Editor />
       <LeftRail />
+      <RightRail />
     </div>
   );
 }

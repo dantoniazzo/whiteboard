2e5a36efe255b68f3592db12546e691aabe83b6e
dantoniazzo <85890036+dantoniazzo@users.noreply.github.com>
2025-03-29 14:58:49 +0100

    Feature: update RightRail component to adjust positioning with translate-x utility

--- src/_widgets/RightRail/ui/RightRail.tsx
+++ src/_widgets/RightRail/ui/RightRail.tsx
@@ -1,7 +1,7 @@
 export const RightRail = () => {
   return (
     <div
-      className={`absolute top-0 right-0 transition w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 h-full bg-gray-900`}
+      className={`absolute top-0 right-0 translate-x-full md:translate-x-0 transition w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 h-full bg-gray-900`}
     ></div>
   );
 };

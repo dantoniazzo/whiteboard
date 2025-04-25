000de3e8b6b49b365f1e2e686f7d5248770b0435
Dorian Antoniazzo <dorian.antoniazzo@bebabit.com>
2025-04-25 16:21:44 +0200

    Feature: remove RightRail component and related files; refactor line creation logic

--- src/_features/line/model/line.types.ts
+++ src/_features/line/model/line.types.ts
@@ -2,8 +2,3 @@ import { Position } from "_shared";
 
 // This tuple type requires at least 2 elements and at most 3 elements
 export type Points = [Position, Position] | [Position, Position, Position];
-
-export interface ILine {
-  id: string;
-  points: Points;
-}
